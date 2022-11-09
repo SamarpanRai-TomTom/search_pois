3e4192062e58e77792bb194418d8a9cce2438657
Toni Almagro <toni.almagro@tomtom.com>
2022-11-09 13:22:00 +0100

    adding query bing as pipeline

--- src/search_pois/pipeline_registry.py
+++ src/search_pois/pipeline_registry.py
@@ -20,4 +20,5 @@ def register_pipelines() -> Dict[str, Pipeline]:
                     ppl['split_ppl'] +
                     ppl["query_bing"],
         'reverse_geo': ppl["rev_geo_ppl"],
+        'query_bing': ppl["query_bing"]
         }
